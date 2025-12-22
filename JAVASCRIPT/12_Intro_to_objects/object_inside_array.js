1568c211f2a3ee5b89b53280b44470be915a1bc4
sakshimudgul <sakshimudgul8652@gmail.com>
2025-12-22 15:47:48 +0530

    js objects

---
+++ JAVASCRIPT/12_Intro_to_objects/object_inside_array.js
@@ -0,0 +1,31 @@
+// Objects inside Array
+
+// Very Useful in Real world Applications
+
+const users = [
+    {
+    userid : 1,
+    user_name: "Yatin",
+    gender : "male"},
+    {
+    userid : 2,
+    user_name: "siddesh",
+    gender : "male"},
+    {
+    userid : 3,
+    user_name: "shivani",
+    gender : "female"},
+
+]
+
+console.log(users);
+
+// Iterating it :
+
+for(let user of users){
+   
+    console.log(user.user_name);
+    console.log(user.userid);
+     console.log(user.gender);
+     console.log(user);
+}
